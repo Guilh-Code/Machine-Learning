9e042532c99d23fd282ea4f617e965d14b8b8c31
Guilherme Rodrigues <GuilhermeRAR2005@gmail.com>
2026-01-08 16:14:56 -0300

    Regressão Linear vs Árvore de Decisões

--- regressao.py
+++ regressao.py
@@ -15,12 +15,19 @@ y = df["nota"] # Isso é um vetor (Series)
 reg = linear_model.LinearRegression()
 reg.fit(X, y)
 
-# %%
-
 a, b = reg.intercept_, reg.coef_[0]
-print(a, b)
 
-predict = reg.predict(X.drop_duplicates())
+predict_reg = reg.predict(X.drop_duplicates())
+
+from sklearn import tree
+
+arvore_full = tree.DecisionTreeRegressor(random_state=42)
+arvore_full.fit(X, y)
+predict_arvore_full = arvore_full.predict(X.drop_duplicates())
+
+arvore_d2 = tree.DecisionTreeRegressor(random_state=42, max_depth=2)
+arvore_d2.fit(X, y)
+predict_arvore_d2 = arvore_d2.predict(X.drop_duplicates())
 
 # %%
 
@@ -32,7 +39,22 @@ plt.title("Relação Cerveja vs Nota")
 plt.xlabel("Cerveja")
 plt.ylabel("Nota")
 
-plt.plot(X.drop_duplicates()["cerveja"], predict)
-plt.legend(["Observado", f"y = {a:.3f} + {b:.3f} X"])
+plt.plot(X.drop_duplicates()["cerveja"], predict_reg)
+plt.plot(X.drop_duplicates()["cerveja"], predict_arvore_full, color="green")
+plt.plot(X.drop_duplicates()["cerveja"], predict_arvore_d2, color="magenta")
+
+plt.legend(["Observado", 
+            f"y = {a:.3f} + {b:.3f} X", 
+            "Árvore Full",
+            "Árvore Depth = 2"
+            ])
+
+# %%
+
+plt.figure(dpi=400)
+
+tree.plot_tree(arvore_d2,
+               feature_names=["cerveja"],
+               filled=True)
 
 # %%
